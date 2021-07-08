30aad579312ae14830b435bc8254f12dd8868bc4
github username <github email address>
2021-07-08 07:16:34 -0400

    Practice working with classes in ruby

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram 
+  attr_accessor :string
+  def initialize(string)
+    @string = string
+  end
+
+  def match(possible_anagrams_array)
+    possible_anagrams_array.select do |elem| 
+      elem.downcase.split("").sort.join("") == @string.downcase.split("").sort.join("")
+    end
+      
+  end
+end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
